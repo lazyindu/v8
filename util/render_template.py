335ed1bf6a97ba997913d603df9387493e01a8e7
indu <70087467+lazyindu@users.noreply.github.com>
2023-08-14 22:04:26 +0530

    Update render_template.py

--- util/render_template.py
+++ util/render_template.py
@@ -29,17 +29,17 @@ async def render_page(file_id):
     video_formats = ['video/mp4', 'video/avi', 'video/ogg', 'video/h264', 'video/h265', 'video/x-matroska']
     try:
         if mime_type.lower() in video_formats:
-            async with aiofiles.open('template/req.html') as r:
+            async with aiofiles.open('req.html') as r:
                 heading = 'Watch {}'.format(file_name)
                 tag = mime_type.split('/')[0].strip()
                 html = (await r.read()).replace('tag', tag) % (heading, file_name, src)
         elif mime_type.lower() in audio_formats:
-            async with aiofiles.open('template/req.html') as r:
+            async with aiofiles.open('/') as r:
                 heading = 'Listen {}'.format(file_name)
                 tag = mime_type.split('/')[0].strip()
                 html = (await r.read()).replace('tag', tag) % (heading, file_name, src)
         else:
-            async with aiofiles.open('template/dl.html') as r:
+            async with aiofiles.open('/template/dl.html') as r:
                 async with aiohttp.ClientSession() as s:
                     async with s.get(src) as u:
                         file_size = human_size(u.headers.get('Content-Length'))
