065e7f98689a0e4841726aba351f1904deb0bfec
indu <70087467+lazyindu@users.noreply.github.com>
2023-08-14 21:14:31 +0530

    Update render_template.py

--- util/render_template.py
+++ util/render_template.py
@@ -9,9 +9,11 @@ import mimetypes
 import aiofiles
 import logging
 import aiohttp
+from database.ia_filterdb import  get_file_details
 
-async def fetch_properties(message_id):
-    media_msg = await StreamBot.get_messages(LOG_CHANNEL, message_id)
+async def fetch_properties(file_id):
+    # media_msg = await StreamBot.get_messages(LOG_CHANNEL, message_id)
+    media_msg = await get_file_details(file_id)
     file_properties = await TGCustomYield().generate_file_properties(media_msg)
     file_name = file_properties.file_name if file_properties.file_name \
         else f"{secrets.token_hex(2)}.jpeg"
@@ -20,25 +22,30 @@ async def fetch_properties(message_id):
     return file_name, mime_type
 
 
-async def render_page(message_id):
-    file_name, mime_type = await fetch_properties(message_id)
-    src = urllib.parse.urljoin(URL, str(message_id))
+async def render_page(file_id):
+    file_name, mime_type = await fetch_properties(file_id)
+    src = urllib.parse.urljoin(URL, str(file_id))
     audio_formats = ['audio/mpeg', 'audio/mp4', 'audio/x-mpegurl', 'audio/vnd.wav']
     video_formats = ['video/mp4', 'video/avi', 'video/ogg', 'video/h264', 'video/h265', 'video/x-matroska']
-    if mime_type.lower() in video_formats:
-        async with aiofiles.open('template/req.html') as r:
-            heading = 'Watch {}'.format(file_name)
-            tag = mime_type.split('/')[0].strip()
-            html = (await r.read()).replace('tag', tag) % (heading, file_name, src)
-    elif mime_type.lower() in audio_formats:
-        async with aiofiles.open('template/req.html') as r:
-            heading = 'Listen {}'.format(file_name)
-            tag = mime_type.split('/')[0].strip()
-            html = (await r.read()).replace('tag', tag) % (heading, file_name, src)
-    else:
-        async with aiofiles.open('template/dl.html') as r:
-            async with aiohttp.ClientSession() as s:
-                async with s.get(src) as u:
-                    file_size = human_size(u.headers.get('Content-Length'))
-                    html = (await r.read()) % (heading, file_name, src, file_size)
+    try:
+        if mime_type.lower() in video_formats:
+            async with aiofiles.open('template/req.html') as r:
+                heading = 'Watch {}'.format(file_name)
+                tag = mime_type.split('/')[0].strip()
+                html = (await r.read()).replace('tag', tag) % (heading, file_name, src)
+        elif mime_type.lower() in audio_formats:
+            async with aiofiles.open('template/req.html') as r:
+                heading = 'Listen {}'.format(file_name)
+                tag = mime_type.split('/')[0].strip()
+                html = (await r.read()).replace('tag', tag) % (heading, file_name, src)
+        else:
+            async with aiofiles.open('template/dl.html') as r:
+                async with aiohttp.ClientSession() as s:
+                    async with s.get(src) as u:
+                        file_size = human_size(u.headers.get('Content-Length'))
+                        html = (await r.read()) % (heading, file_name, src, file_size)
+    except Exception as e:
+        # Print or log the error for debugging
+        print("Error:", e)
+        html = "<p>Error occurred while rendering the page</p>"
     return html
